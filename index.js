7a49862f3f2a974fdcd10d8b0d29f81b237a5bdc
choinashil <choinashil@gmail.com>
2021-11-17 18:51:54 +0900

    change port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express'),
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 80, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 8000, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
