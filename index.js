1bc4e4d8ae2717a0da084dedd5ec089694fda882
choinashil <choinashil@gmail.com>
2021-11-17 18:51:06 +0900

    change port

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const express = require('express'),
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 80, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
-  let body = req.body;
+  const body = req.body;
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
@@ -18,7 +18,7 @@ app.post('/webhook', (req, res) => {
     body.entry.forEach(function (entry) {
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
+      const webhook_event = entry.messaging[0];
       console.log(webhook_event);
     });
 
@@ -33,12 +33,12 @@ app.post('/webhook', (req, res) => {
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = 'token';
+  const VERIFY_TOKEN = 'token';
 
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
 
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
